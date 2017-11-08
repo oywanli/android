ecab6e16ae3585d0cca578fb6544dc9f7e6a6833
believemeng <945858825@qq.com>
2017-11-08 09:20:13 +0000

    Update CommandDownlink.java

--- java/test/com/dspread/pos/sdk/CommandDownlink.java
+++ java/test/com/dspread/pos/sdk/CommandDownlink.java
@@ -4,9 +4,9 @@ package com.dspread.pos.sdk;
 public class CommandDownlink {
 	private Packet p = null;
 
-	public CommandDownlink(int cmdCode, int cmdSubCode, int delay, byte[] paras) {
+	public CommandDownlink(int cmdID,int cmdCode, int cmdSubCode, int delay, byte[] paras) {
 		p = new Packet(paras.length);
-		p.setCmdID((byte) 0x21);
+		p.setCmdID((byte) cmdID);
 		p.setCmdCode((byte) cmdCode);
 		p.setCmdSubCode((byte) cmdSubCode);
 		p.setTimeOut((byte) delay);
