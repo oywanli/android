c181647bbb293db2aec700136918ea270bae9f81
believemeng <945858825@qq.com>
2018-04-17 17:20:35 +0800

    add the android studio demo

---
+++ android studio project/pos_android_demo/app/src/main/java/com/dspread/demoui/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.dspread.demoui;
+
+import com.dspread.demoui.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+		TextView content = (TextView) findViewById(R.id.content);
+//		content.setText("当前版本号："+QPOSService.getSdkVersion()+"\n日期："+QPOSService.getSdkDate()+"\n主要解决问题：增加USB OTG通信方式\n");
+	}
+}
