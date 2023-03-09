569c3ae30cf4909f7ae547b40e9a6fcac5a3b31a
ma <823163940@qq.com>
2023-03-09 11:57:13 +0800

    v4.3.0 - fix the trace bug on android version <7

--- pos_android_studio_demo/pos_android_studio_app/src/main/java/com/dspread/demoui/utils/DateUtils.java
+++ pos_android_studio_demo/pos_android_studio_app/src/main/java/com/dspread/demoui/utils/DateUtils.java
@@ -1,5 +1,7 @@
 package com.dspread.demoui.utils;
 
+import android.os.Build;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
@@ -11,8 +13,12 @@ import java.util.TimeZone;
 public class DateUtils {
 
     public static String getNowTime() {
-
-        SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss:SSS"); //set time format
+        SimpleDateFormat formatter;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            formatter = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss:SSS"); //set time format
+        } else {
+            formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS"); //set time format
+        }
 
         formatter.setTimeZone(TimeZone.getTimeZone("GMT+08")); //set time zone
 
